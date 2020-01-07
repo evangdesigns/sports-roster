eabcdfe556e89ddc7fba1ae3705818d3fb4fb818
Evan Grabenstein <evangdesigns@gmail.com>
2020-01-06 20:06:00 -0600

    printing players on team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Player from '../Player/Player';
+import authData from '../../helpers/data/authData';
+import playerData from '../../helpers/data/playerData';
 import './Team.scss';
 
 class Team extends React.Component {
@@ -6,16 +9,31 @@ class Team extends React.Component {
 
   // }
 
-  // state = {
+  state = {
+    players: [],
+  }
 
-  // }
+  getPlayerData = () => {
+    playerData.getPlayersByUid(authData.getUid())
+      .then((players) => {
+        this.setState({ players });
+      })
+      .catch((errorFromGetPlayers) => console.error({ errorFromGetPlayers }));
+  }
 
-  // Functions
+  componentDidMount() {
+    this.getPlayerData();
+  }
+
   render() {
-    // variables
+    const { players } = this.state;
+
     return (
       <div className="Team">
         <h1>SUPER DUPPER STORM TROOPERS</h1>
+        <div className="d-flex flex-wrap justify-content-center">
+          {players.map((player) => (<Player key={player.id} player={player} />))}
+        </div>
       </div>
     );
   }
---
+++ src/helpers/data/playerData.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getPlayersByUid = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/players.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((result) => {
+      const allPlayersObj = result.data;
+      const players = [];
+      if (allPlayersObj != null) {
+        Object.keys(allPlayersObj).forEach((playerId) => {
+          const newPlayer = allPlayersObj[playerId];
+          newPlayer.id = playerId;
+          players.push(newPlayer);
+        });
+      }
+      resolve(players);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
+const getSinglePlayer = (playerId) => axios.get(`${baseUrl}/playerss/${playerId}.json`);
+
+const deletePlayer = (playerId) => axios.delete(`${baseUrl}/players/${playerId}.json`);
+
+const addPlayer = (playerInfo) => axios.post(`${baseUrl}/players.json`, playerInfo);
+
+export default {
+  getPlayersByUid,
+  getSinglePlayer,
+  deletePlayer,
+  addPlayer,
+};
